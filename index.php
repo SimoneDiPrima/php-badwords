061b1cd0f28846646fc6490b366344b27e3fb37d
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-08-29 15:25:45 +0200

    variabile paragrafo e lunghezza paragrafo

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>pippo</h1>
-</body>
-</html>
+<?php 
+$paragraph='Tre tigri contro tre tigri, tre tigri contro tre tigri, tre tigri contro tre tigri';
+echo $paragraph;
+echo '<br/><br/><br/>';
+echo strlen($paragraph);
+?>
