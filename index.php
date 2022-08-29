9486a5dc4d68fbbc6b8b81818b0300a0b1fd64f2
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-08-29 16:28:06 +0200

    sostituzione query con la funzione str_replace

--- index.php
+++ index.php
@@ -6,20 +6,13 @@ echo strlen($paragraph);
 echo '<br/><br/><br/>';
 $numbers=  $_GET['numbers'];
 $species = $_GET['name'];
-$new_song = "$numbers $species"
-
-
+echo $numbers;
+echo '<br/><br/><br/>';
+echo $species;
+$my_replaced_paragraph= str_replace($species,'ippopotami',$paragraph);
+echo '<br/><br/><br/>';
+echo $my_replaced_paragraph;
+$my_replaced_paragraph .= str_replace($numbers,'trentatre',$paragraph);
+echo '<br/><br/><br/>';
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1> <?php echo $new_song ?></h1>
-</body>
-</html>
